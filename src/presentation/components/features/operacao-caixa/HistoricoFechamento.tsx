e06c7f8e307406b72a511d140ced411f685c101f
Andre Silva <hardsys_andre@hotmail.com>
2025-12-22 16:41:05 -0300

    fix: pequeno ajuste na listagem de historico de fechamento de caixa

--- src/presentation/components/features/operacao-caixa/HistoricoFechamento.tsx
+++ src/presentation/components/features/operacao-caixa/HistoricoFechamento.tsx
@@ -796,12 +796,12 @@ export function HistoricoFechamento() {
         </div>
 
         {/* Cabeçalho da Tabela */}
-        <div className="bg-primary px-3 py-2 flex items-center text-info text-sm font-nunito font-semibold">
+        <div className="bg-custom-2 px-3 py-2 rounded-t-lg flex items-center text-primary-text text-sm font-nunito font-semibold">
           <div className="flex-1">Cód. Terminal</div>
-          <div className="flex-1">Terminal</div>
-          <div className="flex-1">Fechado por</div>
-          <div className="flex-1">Data Abertura</div>
-          <div className="flex-1">Data Fechamento</div>
+          <div className="flex-[1.5]">Terminal</div>
+          <div className="flex-[2]">Fechado por</div>
+          <div className="flex-1">Dt. Abertura</div>
+          <div className="flex-1">Dt. Fechamento</div>
           <div className="flex-1 text-center">Período Aberto</div>
           <div className="flex-1 text-center">Status</div>
         </div>
@@ -834,12 +834,12 @@ export function HistoricoFechamento() {
                       setSelectedOperacaoId(operacao.id)
                       setIsDetalhesModalOpen(true)
                     }}
-                    className="mx-2 my-1 px-3 py-2 rounded-lg bg-info cursor-pointer transition-all hover:bg-primary-background hover:shadow-md"
+                    className="mx-2 my-1 p-3 rounded-lg bg-info cursor-pointer transition-all hover:bg-primary/10 shadow-lg "
                   >
                     <div className="flex items-center text-sm font-nunito text-primary-text">
                       <div className="flex-1">{operacao.codigoTerminal || '-'}</div>
-                      <div className="flex-1">{operacao.nomeTerminal || '-'}</div>
-                      <div className="flex-1">{operacao.nomeResponsavelFechamento || '-'}</div>
+                      <div className="flex-[1.5]">{operacao.nomeTerminal || '-'}</div>
+                      <div className="flex-[2]">{operacao.nomeResponsavelFechamento || '-'}</div>
                       <div className="flex-1">
                         {dataAbertura.date}, {dataAbertura.time}
                       </div>
